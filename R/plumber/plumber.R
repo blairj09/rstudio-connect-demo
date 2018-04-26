90728bf26fccd754fa31c76f76d5075ebcdc40ff
James Blair <james.m.blair09@gmail.com>
2018-04-26 01:01:17 -0600

    Add image endpoint to plumber.R

--- R/plumber/plumber.R
+++ R/plumber/plumber.R
@@ -4,8 +4,11 @@
 
 # This provides a simple example of an API created with the plumber package
 
+# Packages
+library(tidyverse)
+
 # Load existing model
-# simple_model <- readRDS("simple-model.rds")
+simple_model <- readRDS("simple-model.rds")
 
 # Create API endpoint
 
@@ -14,11 +17,27 @@
 #* @param cyl Cylinder
 #* @get /mpgpred
 mpg_pred <- function(hp, cyl) {
-  simple_model <- lm(mpg ~ hp + cyl, data = mtcars)
-  
   predict(simple_model,
           data.frame(
             hp = as.numeric(hp),
             cyl = as.numeric(cyl)
           ))
+}
+
+#* Return a plot of filtered data
+#* @param cyl Cylinder value to filter mtcars to
+#* @jpeg
+#* @get /mpgplot
+mpg_plot <- function(cyl) {
+  plot_data <- mtcars
+  if (!missing(cyl)) {
+    cyl_filter <- as.numeric(cyl)
+    plot_data <- mtcars %>% 
+      filter(cyl == cyl_filter)
+  }
+  plot_data %>% 
+    filter(cyl == cyl_filter) %>% 
+    ggplot(aes(x = hp, y = mpg)) +
+    geom_point() +
+    theme_bw()
 }
